3018eab6a1f7cda2bc80be81496aa5f4fd40d5cf
saber4869 <saber4869@outlook.com>
2016-10-14 10:57:37 +0800

    some change

--- src/com/aisino/finance/aisinopay/web/controller/HomePageController.java
+++ src/com/aisino/finance/aisinopay/web/controller/HomePageController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+ * Ê×Ò³Ìø×ª¿ØÖÆ
+ *
+ */
+
 @Controller
 @RequestMapping("/home")
 public class HomePageController {
