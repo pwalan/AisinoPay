48419e821c54aea2bdfb2e6ed5f3b89bbedf6f20
AlanP <pwalan@foxmail.com>
2016-10-24 14:31:49 +0800

    add hibernate and login

--- src/com/aisino/finance/aisinopay/pojo/User.java
+++ src/com/aisino/finance/aisinopay/pojo/User.java
@@ -1,5 +1,59 @@
 package com.aisino.finance.aisinopay.pojo;
 
-public class User {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 用户表映射实体类
+ * @author AlanP
+ *
+ */
+@Entity
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int uid;
+	private String username;
+	private String passwd;
+	
+	public User() {
+	}
+	
+	public User(String username, String passwd) {
+		this.username = username;
+		this.passwd = passwd;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	
 }
--- src/com/aisino/finance/aisinopay/web/controller/UserContorller.java
+++ src/com/aisino/finance/aisinopay/web/controller/UserContorller.java
@@ -1,5 +1,49 @@
 package com.aisino.finance.aisinopay.web.controller;
 
-public class UserContorller {
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.aisino.finance.aisinopay.dao.IUserDao;
+import com.aisino.finance.aisinopay.pojo.User;
+
+/**
+ * 用户登录注册跳转控制
+ * @author AlanP
+ *
+ */
+@Controller
+@RequestMapping("/user")
+public class UserContorller {
+	
+	@Resource
+	private IUserDao userDao;
+	
+	public IUserDao getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(IUserDao userDao) {
+		this.userDao = userDao;
+	}
+
+
+
+	@RequestMapping("/login")
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView mav=new ModelAndView();
+		
+		String username=request.getParameter("username");
+		String passwd=request.getParameter("passwd");
+		System.out.println(username+"-"+passwd);
+		User user=userDao.findUserByUsername(username);
+		if(user!=null&&user.getPasswd().equals(passwd)){
+			System.out.println("success");
+		}
+		return mav;
+	}
 }
